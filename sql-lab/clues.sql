efbb2b1365817a839572fc484d1ad4f4a4bed8ed
David Aydin <david.aydin6@gmail.com>
2022-06-27 23:11:12 -0400

    Clue 7 Query

--- sql-lab/clues.sql
+++ sql-lab/clues.sql
@@ -87,3 +87,7 @@ ORDER BY population;
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+SELECT *
+FROM city
+WHERE population=91084;
+-- Santa Monica USA California
