d5c6346f8f9f09d7ae0d783e5787e009f0e85921
David Aydin <david.aydin6@gmail.com>
2022-06-27 23:00:25 -0400

    Clue 5 Query

--- sql-lab/clues.sql
+++ sql-lab/clues.sql
@@ -52,7 +52,12 @@ ORDER BY population;
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-
+SELECT *
+FROM country
+    LEFT JOIN countrylanguage ON country.code=countrylanguage.countrycode
+WHERE region='South America' AND language='Italian'
+ORDER BY population;
+-- Query Top Response: Argentina or Brazil
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
